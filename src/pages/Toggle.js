7c01ecb12d7f42d367185fa3d24b824f4c4659f4
Roberto Vázquez González <robertovg24@gmail.com>
2020-02-27 21:46:30 +0100

    We have the toggle, 1 of 3

--- src/pages/Toggle.js
+++ src/pages/Toggle.js
@@ -1,8 +1,42 @@
 import React from 'react'
+import { useMachine } from '@xstate/react'
+import { Machine } from 'xstate'
 import Layout from '../components/Layout'
 
-const Toggle = () => {
-  return <Layout>Toogle Page</Layout>
-}
+const toggleMachine = new Machine({
+  id: 'toggleMachine',
+  initial: 'inactive',
+  states: {
+    inactive: {
+      on: {
+        TOGGLE: 'active',
+      },
+    },
+    active: {
+      on: {
+        TOGGLE: 'inactive',
+      },
+    },
+  },
+})
 
+function Toggle() {
+  const [current, send] = useMachine(toggleMachine, { devTools: true })
+
+  return (
+    <Layout>
+      <button
+        type="button"
+        onClick={() => {
+          send('TOGGLE')
+        }}
+      >
+        Toggle
+      </button>
+
+      {current.matches('active') && <span>We are active</span>}
+      {current.matches('inactive') && <span>We are inactive</span>}
+    </Layout>
+  )
+}
 export default Toggle
